e2e8de81def43cae7d369eaf15b5a1e7581f57e9
meettyj <meettianyijun@gmail.com>
2019-10-26 23:00:22 -0400

    update for AWM_Rac_tweets

--- R/regression_discrimination_AWM_Rac_tweets.R
+++ R/regression_discrimination_AWM_Rac_tweets.R
@@ -24,112 +24,113 @@ merged_survey_discrimination_data <- merge(survey_data,discrimination_data,by.x 
 merged_survey_discrimination_data
 
 
+
 # ---- Poisson ----
 # AI
-glm_poisson_AI_Total <- glm(AI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_AI_Total <- glm(AI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_AI_Total)
 
-glm_poisson_AI_Condom <- glm(AI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_AI_Condom <- glm(AI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_AI_Condom)
 
-glm_poisson_AI_Condomless <- glm(AI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_AI_Condomless <- glm(AI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_AI_Condomless)
 
 # IAI
-glm_poisson_IAI_Total <- glm(IAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_IAI_Total <- glm(IAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_IAI_Total)
 
-glm_poisson_IAI_Condom <- glm(IAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_IAI_Condom <- glm(IAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_IAI_Condom)
 
-glm_poisson_IAI_Condomless <- glm(IAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_IAI_Condomless <- glm(IAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_IAI_Condomless)
 
 # RAI
-glm_poisson_RAI_Total <- glm(RAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_RAI_Total <- glm(RAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_RAI_Total)
 
-glm_poisson_RAI_Condom <- glm(RAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_RAI_Condom <- glm(RAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_RAI_Condom)
 
-glm_poisson_RAI_Condomless <- glm(RAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_RAI_Condomless <- glm(RAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_RAI_Condomless)
 
 # Drug use
-glm_poisson_drug_use <- glm(drug_use ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+glm_poisson_drug_use <- glm(drug_use ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
 summary(glm_poisson_drug_use)
 
 
 
 # ---- Negative Binomial ----
 # AI
-glm_nb_AI_Total <- glm.nb(AI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_AI_Total <- glm.nb(AI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_AI_Total)
 
-glm_nb_AI_Condom <- glm.nb(AI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_AI_Condom <- glm.nb(AI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_AI_Condom)
 
-glm_nb_AI_Condomless <- glm.nb(AI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_AI_Condomless <- glm.nb(AI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_AI_Condomless)
 
 # IAI
-glm_nb_IAI_Total <- glm.nb(IAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_IAI_Total <- glm.nb(IAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_IAI_Total)
 
-glm_nb_IAI_Condom <- glm.nb(IAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_IAI_Condom <- glm.nb(IAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_IAI_Condom)
 
-glm_nb_IAI_Condomless <- glm.nb(IAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_IAI_Condomless <- glm.nb(IAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_IAI_Condomless)
 
 # RAI
-glm_nb_RAI_Total <- glm.nb(RAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_RAI_Total <- glm.nb(RAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_RAI_Total)
 
-glm_nb_RAI_Condom <- glm.nb(RAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_RAI_Condom <- glm.nb(RAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_RAI_Condom)
 
-glm_nb_RAI_Condomless <- glm.nb(RAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_RAI_Condomless <- glm.nb(RAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_RAI_Condomless)
 
 # Drug use
-glm_nb_drug_use <- glm.nb(drug_use ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+glm_nb_drug_use <- glm.nb(drug_use ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, data=merged_survey_discrimination_data)
 summary(glm_nb_drug_use)
 
 
 # ---- Quasi Poisson ----
 # AI
-glm_quasi_poisson_AI_Total <- glm(AI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_AI_Total <- glm(AI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_AI_Total)
 
-glm_quasi_poisson_AI_Condom <- glm(AI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_AI_Condom <- glm(AI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_AI_Condom)
 
-glm_quasi_poisson_AI_Condomless <- glm(AI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_AI_Condomless <- glm(AI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_AI_Condomless)
 
 # IAI
-glm_quasi_poisson_IAI_Total <- glm(IAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_IAI_Total <- glm(IAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_IAI_Total)
 
-glm_quasi_poisson_IAI_Condom <- glm(IAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_IAI_Condom <- glm(IAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_IAI_Condom)
 
-glm_quasi_poisson_IAI_Condomless <- glm(IAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_IAI_Condomless <- glm(IAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_IAI_Condomless)
 
 # RAI
-glm_quasi_poisson_RAI_Total <- glm(RAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_RAI_Total <- glm(RAI_Total ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_RAI_Total)
 
-glm_quasi_poisson_RAI_Condom <- glm(RAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_RAI_Condom <- glm(RAI_Condom ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_RAI_Condom)
 
-glm_quasi_poisson_RAI_Condomless <- glm(RAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_RAI_Condomless <- glm(RAI_Condomless ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_RAI_Condomless)
 
 # Drug use
-glm_quasi_poisson_drug_use <- glm(drug_use ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Zip_Rac, family=quasipoisson,data=merged_survey_discrimination_data)
+glm_quasi_poisson_drug_use <- glm(drug_use ~ agef1y+racefa1_b+eduf3+eduf1+bornusf1_b+income_su+AWM_Rac_tweets, family=quasipoisson,data=merged_survey_discrimination_data)
 summary(glm_quasi_poisson_drug_use)
 
 
