2176a40959acf445f5ea482d6015e9b1158cad24
meettyj <meettianyijun@gmail.com>
2019-10-29 19:23:04 -0400

    update for alcohf6

---
+++ R/regression_alcohf6.R
@@ -0,0 +1,137 @@
+require(MASS)
+require(xlsx)
+# install.packages("MASS")
+# install.packages("xlsx")
+
+# Set the working environment here
+setwd("C:/Users/eddie/Desktop/Prof. Rumi Chunara/2019_Fall/msm-discrimination-on-twitter/")
+
+survey_data_path <- "data/"
+survey_data_file <- "P18_Final_Data_07162019.csv"
+discrimination_data_path <- "data/discrimination/"
+discrimination_data_file <- "P18_GPS_AWM_Twitter_data_summarystats.xlsx"
+
+survey_data <- read.csv(paste(survey_data_path,survey_data_file,sep=""),header=TRUE)
+# survey_data_covariates <- survey_data[c("a_pid","agef1y","racefa1_b","eduf3","eduf1","bornusf1_b","income_su")]
+# survey_data_outcomes <- survey_data[c("AI_Total", "AI_Condom", "AI_Condomless")]
+
+discrimination_data <- read.xlsx(paste(discrimination_data_path,discrimination_data_file,sep=""),sheetIndex=1,colNames = TRUE)
+discrimination_data
+#test_data <- survey_data[c('a_pid','agef1y','racefa1_b')]
+#test_data
+
+merged_survey_discrimination_data <- merge(survey_data,discrimination_data,by.x = 'a_pid', by.y = 'PID')
+merged_survey_discrimination_data
+
+# remove NA value in income_su
+merged_survey_discrimination_data$income_su
+which(merged_survey_discrimination_data$income_su =="NA")
+merged_survey_discrimination_data <- merged_survey_discrimination_data[which(merged_survey_discrimination_data$income_su !="NA"), ]
+merged_survey_discrimination_data$income_su
+
+
+# convert age
+merged_survey_discrimination_data['agef1y'] <- 2019 - merged_survey_discrimination_data$agef1y
+merged_survey_discrimination_data['agef1y']
+
+# add dummy variable column for race: racefa1_b
+merged_survey_discrimination_data$race_hispanic <- merged_survey_discrimination_data$racefa1_b
+merged_survey_discrimination_data$race_black <- merged_survey_discrimination_data$racefa1_b
+merged_survey_discrimination_data$race_asian <- merged_survey_discrimination_data$racefa1_b
+merged_survey_discrimination_data$race_mixed <- merged_survey_discrimination_data$racefa1_b
+merged_survey_discrimination_data$race_other <- merged_survey_discrimination_data$racefa1_b
+merged_survey_discrimination_data$race_white <- merged_survey_discrimination_data$racefa1_b
+
+merged_survey_discrimination_data$race_hispanic[merged_survey_discrimination_data$race_hispanic != 1]<-0
+
+merged_survey_discrimination_data$race_black[merged_survey_discrimination_data$race_black != 2]<-0
+merged_survey_discrimination_data$race_black[merged_survey_discrimination_data$race_black == 2]<-1
+
+merged_survey_discrimination_data$race_asian[merged_survey_discrimination_data$race_asian != 3]<-0
+merged_survey_discrimination_data$race_asian[merged_survey_discrimination_data$race_asian == 3]<-1
+
+merged_survey_discrimination_data$race_mixed[merged_survey_discrimination_data$race_mixed != 4]<-0
+merged_survey_discrimination_data$race_mixed[merged_survey_discrimination_data$race_mixed == 4]<-1
+
+# merged_survey_discrimination_data$race_other[merged_survey_discrimination_data$race_other != 5]<-0
+# merged_survey_discrimination_data$race_other[merged_survey_discrimination_data$race_other == 5]<-1
+
+merged_survey_discrimination_data$race_white[merged_survey_discrimination_data$race_white != 6]<-0
+merged_survey_discrimination_data$race_white[merged_survey_discrimination_data$race_white == 6]<-1
+
+# merged_survey_discrimination_data$race_hispanic
+# merged_survey_discrimination_data$race_black
+# merged_survey_discrimination_data$race_asian
+# merged_survey_discrimination_data$race_mixed
+# merged_survey_discrimination_data$race_other
+# merged_survey_discrimination_data$race_white
+
+
+# add dummy variable column for income: income_su
+merged_survey_discrimination_data$income_su
+
+
+merged_survey_discrimination_data$income_low <- merged_survey_discrimination_data$income_su
+merged_survey_discrimination_data$income_medium <- merged_survey_discrimination_data$income_su
+merged_survey_discrimination_data$income_high <- merged_survey_discrimination_data$income_su
+
+merged_survey_discrimination_data$income_low[merged_survey_discrimination_data$income_low != 1]<-0
+
+merged_survey_discrimination_data$income_medium[merged_survey_discrimination_data$income_medium != 2]<-0
+merged_survey_discrimination_data$income_medium[merged_survey_discrimination_data$income_medium == 2]<-1
+
+merged_survey_discrimination_data$income_high[merged_survey_discrimination_data$income_high != 3]<-0
+merged_survey_discrimination_data$income_high[merged_survey_discrimination_data$income_high == 3]<-1
+
+merged_survey_discrimination_data$income_low
+merged_survey_discrimination_data$income_medium
+merged_survey_discrimination_data$income_high
+
+
+# ---- Poisson Regression ----
+glm_poisson_alcohf6 <- glm(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium, family="poisson",data=merged_survey_discrimination_data)
+summary(glm_poisson_alcohf6)
+
+# discrimination variables
+glm_poisson_alcohf6_AWM_Rac_tweets <- glm(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_Rac_tweets, family="poisson",data=merged_survey_discrimination_data)
+summary(glm_poisson_alcohf6_AWM_Rac_tweets)
+
+glm_poisson_alcohf6_AWM_SSSOM_Hom <- glm(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_SSSOM_Hom, family="poisson",data=merged_survey_discrimination_data)
+summary(glm_poisson_alcohf6_AWM_SSSOM_Hom)
+
+glm_poisson_alcohf6_AWM_SSSOM_Rac <- glm(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_SSSOM_Rac, family="poisson",data=merged_survey_discrimination_data)
+summary(glm_poisson_alcohf6_AWM_SSSOM_Rac)
+
+glm_poisson_alcohf6_AWM_Zip_Hom <- glm(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_Zip_Hom, family="poisson",data=merged_survey_discrimination_data)
+summary(glm_poisson_alcohf6_AWM_Zip_Hom)
+
+glm_poisson_alcohf6_AWM_Zip_Rac <- glm(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_Zip_Rac, family="poisson",data=merged_survey_discrimination_data)
+summary(glm_poisson_alcohf6_AWM_Zip_Rac)
+
+
+# ---- Negative Binomial ----
+glm_nb_alcohf6 <- glm.nb(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium, data=merged_survey_discrimination_data)
+summary(glm_nb_alcohf6)
+
+# discrimination variables
+glm_nb_alcohf6_AWM_Rac_tweets <- glm.nb(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_Rac_tweets, data=merged_survey_discrimination_data)
+summary(glm_nb_alcohf6_AWM_Rac_tweets)
+
+glm_nb_alcohf6_AWM_SSSOM_Hom <- glm.nb(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_SSSOM_Hom, data=merged_survey_discrimination_data)
+summary(glm_nb_alcohf6_AWM_SSSOM_Hom)
+
+glm_nb_alcohf6_AWM_SSSOM_Rac <- glm.nb(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_SSSOM_Rac, data=merged_survey_discrimination_data)
+summary(glm_nb_alcohf6_AWM_SSSOM_Rac)
+
+glm_nb_alcohf6_AWM_Zip_Hom <- glm.nb(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_Zip_Hom, data=merged_survey_discrimination_data)
+summary(glm_nb_alcohf6_AWM_Zip_Hom)
+
+glm_nb_alcohf6_AWM_Zip_Rac <- glm.nb(alcohf6 ~ agef1y+eduf1+race_hispanic+race_black+race_asian+race_mixed+race_white+income_high+income_medium+AWM_Zip_Rac, data=merged_survey_discrimination_data)
+summary(glm_nb_alcohf6_AWM_Zip_Rac)
+
+
+
+
+
+
+
---
+++ results/regression/alcohf6.md
@@ -0,0 +1,84 @@
+
+# alcohf6
+
+# Poisson Regression
+## general
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/possion_general.png" />
+</p>
+
+## AWM_SSSOM_Rac
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/possion_AWM_SSSOM_Rac.png" />
+</p>
+
+## AWM_SSSOM_Hom
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/possion_AWM_SSSOM_Hom.png" />
+</p>
+
+## AWM_Rac_tweets
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/possion_AWM_Rac_tweets.png" />
+</p>
+
+## AWM_Zip_Rac
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/possion_AWM_Zip_Rac.png" />
+</p>
+
+## AWM_Zip_Hom
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/possion_AWM_Zip_Hom.png" />
+</p>
+
+
+
+# Negative Binomial Regression
+## general
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/NB_general.png" />
+</p>
+
+## AWM_SSSOM_Rac
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/NB_AWM_SSSOM_Rac.png" />
+</p>
+
+## AWM_SSSOM_Hom
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/NB_AWM_SSSOM_Hom.png" />
+</p>
+
+## AWM_Rac_tweets
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/NB_AWM_Rac_tweets.png" />
+</p>
+
+## AWM_Zip_Rac
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/NB_AWM_Zip_Rac.png" />
+</p>
+
+## AWM_Zip_Hom
+<p align="center">
+  <img src="https://github.com/meettyj/MSM-Discrimination-on-Twitter/raw/master/results/screenshots/regression/alcohf6/NB_AWM_Zip_Hom.png" />
+</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
